426e019f0df526e33a2ed424b1a6337c132ce83b
27medwa2el <a7med242@gmail.com>
2023-03-06 13:41:21 +0200

    updated conStr

--- Data/Migrations/20230306113558_upDB.Designer.cs
+++ Data/Migrations/20230306113558_upDB.Designer.cs
@@ -4,6 +4,7 @@ using AirBNB.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 
@@ -12,9 +13,11 @@ using NetTopologySuite.Geometries;
 namespace AirBNB.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230306113558_upDB")]
+    partial class upDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
