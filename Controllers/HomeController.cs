907ace47b450a8ad4f1ca25e0c26a2c7222f4bbc
MoHaMeDGaBeR97 <mohamed.gaber1997@outlook.com>
2023-03-08 14:45:19 +0200

    xv

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,7 +2,6 @@
 using AirBNB.Classes;
 using AirBNB.Data;
 using AirBNB.Models;
-using AirBNB.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -29,32 +28,33 @@ namespace AirBNB.Controllers
 
         public IActionResult PropertyDetails(int id)
         {
+            //Propert
             var Property = db.Properties.Include(p => p.PropertyImages).Include(p => p.Amentios)
-                .Include(p => p.Reviews).Include(p => p.House_Rules).FirstOrDefault(p => p.ID == id);
+                .Include(p => p.Reviews).ThenInclude(r => r.User).Include(p => p.House_Rules).FirstOrDefault(p => p.ID == id);
+            //Cities
             var cities = db.Cities.ToList();
             ViewBag.cities = cities;
+            //Regions
             var PropertyRegion = db.Regions.FirstOrDefault(a => a.Id == Property.RegionId).Name;
             ViewBag.PropertyRegion = PropertyRegion;
+            //Property Imgs
             var PropertyLgImg = db.PropertyImages.FirstOrDefault(a => a.PropertyID == Property.ID).URL;
             ViewBag.PropertyLgImg = PropertyLgImg;
+            //Property Catagory
             var PropertyCatagory = db.Categories.FirstOrDefault(a => a.Id == Property.CatogeryId).Name;
             ViewBag.PropertyCatagory = PropertyCatagory;
+            //Property Host
             var PropertyUserName = db.Users.FirstOrDefault(a => a.Id == Property.UserId);
             ViewBag.PropertyUserName = PropertyUserName.First_Name;
             ViewBag.PropertyUserProfilePic = PropertyUserName.Profile_Picture;
-
-            if (Property == null)
-            {
-                return NotFound();
-            }
-            var detailsandpayment = new DetailsPaymentViewModel
-            {
-                property = db.Properties.Include(p => p.PropertyImages).Include(p => p.Amentios)
-                .Include(p => p.Reviews).ThenInclude(a=>a.User).Include(p => p.House_Rules).FirstOrDefault(p => p.ID == id),
-                reservation = new Reservation()
-                
-            };
-            return View(detailsandpayment);
+            ViewBag.PropertyUserJoinedDate = PropertyUserName.Join_Date.Year;
+            ViewBag.PropertyUserEmail = PropertyUserName.Email;
+            //Property Review
+            var reviewRatings = db.Reviews.Where(r => r.PropertyId == Property.ID).Select(r => r.Rating).Average();
+            ViewBag.reviewRatings = reviewRatings.ToString("0.00");
+            var reviewRatingsCount = db.Reviews.Where(r => r.PropertyId == Property.ID).Select(r => r.Rating).Count();
+            ViewBag.reviewRatingsCount = reviewRatingsCount;
+            return View(Property);
         }
 
     }
